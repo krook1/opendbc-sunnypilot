ceb35c381a8034e5c623a4c0e5a74bdea2fa769d
Comma Device <device@comma.ai>
2026-04-22 20:40:12 +0000

    Added basic changes for JLR EVA2

--- opendbc/car/values.py
+++ opendbc/car/values.py
@@ -15,8 +15,9 @@ from opendbc.car.tesla.values import CAR as TESLA
 from opendbc.car.toyota.values import CAR as TOYOTA
 from opendbc.car.volkswagen.values import CAR as VOLKSWAGEN
 from opendbc.car.bmw.values import CAR as BMW
+from opendbc.car.jlr.values import CAR as JLR
 
-Platform = BODY | CHRYSLER | FORD | GM | HONDA | HYUNDAI | MAZDA | MOCK | NISSAN | PSA | RIVIAN | SUBARU | TESLA | TOYOTA | VOLKSWAGEN | BMW
+Platform = BODY | CHRYSLER | FORD | GM | HONDA | HYUNDAI | MAZDA | MOCK | NISSAN | PSA | RIVIAN | SUBARU | TESLA | TOYOTA | VOLKSWAGEN | BMW | JLR
 BRANDS = get_args(Platform)
 
 PLATFORMS: dict[str, Platform] = {str(platform): platform for brand in BRANDS for platform in brand}
